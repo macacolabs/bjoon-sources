ac85487080d40e8dc7a679006a407f54609ff662
macacolabs <monkey.ohgiraffers@gmail.com>
2025-12-08 22:12:56 +0900

    2557, 1000 clear

---
+++ src/main/java/com/macacolabs/ctest/print/ex1/Main.java
@@ -0,0 +1,10 @@
+package com.macacolabs.ctest.print.ex1;
+
+//https://www.acmicpc.net/problem/2557
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
