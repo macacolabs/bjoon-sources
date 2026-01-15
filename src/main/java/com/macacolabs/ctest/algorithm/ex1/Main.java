d12aaeaa77709c216d4eb7e162fed4f08bfb2185
macacolabs <monkey.ohgiraffers@gmail.com>
2026-01-15 15:46:03 +0900

    24262 clear

---
+++ src/main/java/com/macacolabs/ctest/algorithm/ex1/Main.java
@@ -0,0 +1,11 @@
+package com.macacolabs.ctest.algorithm.ex1;
+// https://www.acmicpc.net/problem/24262
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(1);
+        System.out.println(0);
+    }
+}
