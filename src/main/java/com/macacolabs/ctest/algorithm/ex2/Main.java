1a68d18ae14258a0c93010d4c5a52ae20e052f68
macacolabs <monkey.ohgiraffers@gmail.com>
2026-01-16 20:28:01 +0900

    24263 clear

---
+++ src/main/java/com/macacolabs/ctest/algorithm/ex2/Main.java
@@ -0,0 +1,18 @@
+package com.macacolabs.ctest.algorithm.ex2;
+// https://www.acmicpc.net/problem/24263
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.println(N);
+        System.out.println(1);
+    }
+}
