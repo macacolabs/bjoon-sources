b81904122e863b8f5b79ab45fee613eb80af58d8
macacolabs <monkey.ohgiraffers@gmail.com>
2026-01-18 16:58:11 +0900

    24265 clear

---
+++ src/main/java/com/macacolabs/ctest/algorithm/ex4/Main.java
@@ -0,0 +1,18 @@
+package com.macacolabs.ctest.algorithm.ex4;
+// https://www.acmicpc.net/problem/24265
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        long N = Long.parseLong(br.readLine());
+
+        System.out.println((N * (N - 1)) / 2);
+        System.out.println(2);
+    }
+}
