f602f82913136c0b716aaf7c66534ef700b28e4d
macacolabs <monkey.ohgiraffers@gmail.com>
2026-01-20 20:26:54 +0900

    24267 clear

---
+++ src/main/java/com/macacolabs/ctest/algorithm/ex6/Main.java
@@ -0,0 +1,20 @@
+package com.macacolabs.ctest.algorithm.ex6;
+// https://www.acmicpc.net/problem/24267
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        long n = Long.parseLong(br.readLine());
+
+        System.out.println((n * (n - 1) * (n - 2)) / 6);
+
+        System.out.println(3);
+    }
+}
