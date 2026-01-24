96e2f6821d81800433eef8e02e48f7ef6d0815c7
macacolabs <monkey.ohgiraffers@gmail.com>
2026-01-24 11:58:28 +0900

    19532 clear

---
+++ src/main/java/com/macacolabs/ctest/bruteforce/ex3/Main.java
@@ -0,0 +1,33 @@
+package com.macacolabs.ctest.bruteforce.ex3;
+// https://www.acmicpc.net/problem/19532
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        int f = Integer.parseInt(st.nextToken());
+
+        for(int i = -999; i <= 999; i++){
+            for(int j = -999; j <= 999; j++){
+                if(c == ((a * i) + (b * j)) && f == ((d * i) + (e * j))){
+                    System.out.println(i + " " + j);
+                    return;
+                }
+            }
+        }
+    }
+}
