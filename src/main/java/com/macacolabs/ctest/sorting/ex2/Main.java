b467b60b5e66fc4c7693c239c25d7af2afe7e804
macacolabs <monkey.ohgiraffers@gmail.com>
2026-01-30 09:36:05 +0900

    2587 clear

---
+++ src/main/java/com/macacolabs/ctest/sorting/ex2/Main.java
@@ -0,0 +1,29 @@
+package com.macacolabs.ctest.sorting.ex2;
+// https://www.acmicpc.net/problem/2587
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] iarr = new int[5];
+        int sum = 0;
+
+        for(int i = 0; i < 5; i++) {
+            iarr[i] = Integer.parseInt(br.readLine());
+            sum += iarr[i];
+        }
+
+        Arrays.sort(iarr);
+
+        System.out.println(sum / 5);
+        System.out.println(iarr[2]);
+
+    }
+}
