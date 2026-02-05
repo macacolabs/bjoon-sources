1d201bbe5b7a404df26dd5f1c7457bf366d74d3c
macacolabs <monkey.ohgiraffers@gmail.com>
2026-02-05 09:11:51 +0900

    11651 clear

---
+++ src/main/java/com/macacolabs/ctest/sorting/ex8/Main.java
@@ -0,0 +1,37 @@
+package com.macacolabs.ctest.sorting.ex8;
+// https://www.acmicpc.net/problem/11651
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] arr = new int[N][2];
+        for(int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, (a, b) -> {
+
+           if(a[1] == b[1]) return a[0] - b[0];
+           else return a[1] - b[1];
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < N; i++) {
+            sb.append(arr[i][0] + " " + arr[i][1] + "\n");
+        }
+        System.out.println(sb);
+    }
+}
