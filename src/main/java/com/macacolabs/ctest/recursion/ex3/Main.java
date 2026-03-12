1469298589d0950912d9fba2ed54114f5b1c9b97
macacolabs <monkey.ohgiraffers@gmail.com>
2026-03-12 09:35:59 +0900

    25501 clear

---
+++ src/main/java/com/macacolabs/ctest/recursion/ex3/Main.java
@@ -0,0 +1,32 @@
+package com.macacolabs.ctest.recursion.ex3;
+// https://www.acmicpc.net/problem/25501
+
+import java.io.*;
+
+public class Main {
+    static int count = 0;
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < T; i++) {
+            String str = br.readLine();
+            count = 0;
+            sb.append(isPalindrome(str)).append(" ").append(count).append("\n");
+        }
+        System.out.print(sb);
+    }
+
+    public static int recursion(String s, int l, int r) {
+        count++;
+        if(l >= r) return 1;
+        else if(s.charAt(l) != s.charAt(r)) return 0;
+        else return recursion(s, l + 1, r - 1);
+    }
+
+    public static int isPalindrome(String s) {
+        return recursion(s, 0, s.length() - 1);
+    }
+}
