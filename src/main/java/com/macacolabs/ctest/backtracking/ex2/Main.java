7c3cfe5df86a04fd1574ea01a76d1ed7ee725b24
macacolabs <monkey.ohgiraffers@gmail.com>
2026-03-16 09:42:04 +0900

    15650 clear

---
+++ src/main/java/com/macacolabs/ctest/backtracking/ex2/Main.java
@@ -0,0 +1,44 @@
+package com.macacolabs.ctest.backtracking.ex2;
+// https://www.acmicpc.net/problem/15650
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+
+        dfs(1, 0);
+
+        System.out.print(sb);
+    }
+
+    public static void dfs(int start, int depth) {
+
+        if (depth == M) {
+            for (int val : arr) {
+                sb.append(val).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i <= N; i++) {
+            arr[depth] = i;
+            dfs(i + 1, depth + 1);
+        }
+    }
+ }
