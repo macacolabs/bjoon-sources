9490ee5b30033ecab7270288ebfbb0f4be1c3d1f
macacolabs <monkey.ohgiraffers@gmail.com>
2026-03-25 15:49:55 +0900

    9461 clear

---
+++ src/main/java/com/macacolabs/ctest/dp/ex3/Main.java
@@ -0,0 +1,32 @@
+package com.macacolabs.ctest.dp.ex3;
+
+// https://www.acmicpc.net/problem/9461
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        long[] dp = new long[101];
+
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 1;
+        dp[4] = 2;
+        dp[5] = 2;
+
+        for(int i = 6; i <= 100; i++) {
+
+            dp[i] = dp[i - 1] + dp[i - 5];
+        }
+
+        for(int i = 1; i <= T; i++) {
+            System.out.println(dp[Integer.parseInt(br.readLine())]);
+        }
+    }
+}
