a0a04cb4adb23461710644afada5ae0d34223db0
macacolabs <monkey.ohgiraffers@gmail.com>
2026-03-25 12:30:00 +0900

    1904 clear

--- src/main/java/com/macacolabs/ctest/dp/ex2/Main.java
+++ src/main/java/com/macacolabs/ctest/dp/ex2/Main.java
@@ -1,4 +1,29 @@
 package com.macacolabs.ctest.dp.ex2;
+// https://www.acmicpc.net/problem/1904
+
+import java.io.*;
 
 public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        if(N == 1) {
+            System.out.println(1);
+            return;
+        }
+
+        int[] dp = new int[N + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3; i <= N; i++) {
+
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 15746;
+        }
+
+        System.out.println(dp[N]);
+
+    }
 }
