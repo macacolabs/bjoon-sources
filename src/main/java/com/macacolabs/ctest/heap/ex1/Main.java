9c01d60399da7c4ad2ba64a4afb001c2cb857ef4
macacolabs <monkey.ohgiraffers@gmail.com>
2026-04-18 21:11:11 +0900

    11279 clear

---
+++ src/main/java/com/macacolabs/ctest/heap/ex1/Main.java
@@ -0,0 +1,36 @@
+package com.macacolabs.ctest.heap.ex1;
+// https://www.acmicpc.net/problem/11279
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        StringBuilder sb = new StringBuilder(br.readLine());
+
+        for(int i = 0; i < N; i++) {
+            int x = Integer.parseInt(br.readLine());
+
+            if (x == 0) {
+                if(maxHeap.isEmpty()) {
+                    sb.append(0).append("\n");
+                } else {
+                    sb.append(maxHeap.poll()).append("\n");
+                }
+            } else {
+                maxHeap.add(x);
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
